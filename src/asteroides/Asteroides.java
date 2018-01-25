54b89fe166ec05677bf9cf1e7694e4e31b24f105
isaac <isaac@DESKTOP-G6SCT5K>
2018-01-25 10:31:01 +0100

    nave creada y algo de rotacion

--- src/asteroides/Asteroides.java
+++ src/asteroides/Asteroides.java
@@ -6,46 +6,90 @@
 package asteroides;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.animation.AnimationTimer;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+
 
 /**
  *
  * @author isaac
  */
 public class Asteroides extends Application {
-    
+int propul;
+final int SCENE_TAM_X= 800;
+final int SCENE_TAM_Y= 600; 
+int rotacion=360; //usaremos esta variable para asignar el giro
+
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        Pane root= new Pane();
+        Scene scene = new Scene (root, SCENE_TAM_X, SCENE_TAM_Y);
+        scene.setFill(Color.BLUE);
+        VBox vbox = new VBox();
+        vbox.setLayoutX(800);
+        vbox.setLayoutY(600);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        Polygon nave = new Polygon();
+        nave.getPoints().addAll(new Double[]{
+            0.0, -20.0,
+            -20.0, 20.0,
+            20.0, 20.0 });
+        nave.setFill(Color.YELLOW);
+        nave.getTransforms().add(new Rotate(rotacion,0,00));
+        nave.setTranslateX(400);
+        nave.setTranslateY(350);
+       /* nave.getTransforms().add(new Rotate(30, 50, 30));*/
+        primaryStage.setTitle("Nave Espacial");
         primaryStage.setScene(scene);
         primaryStage.show();
+        root.getChildren().add(nave);
+        //AnimationTimer movimiento;
+       // movimiento = new AnimationTimer(){ 
+            scene.setOnKeyPressed((KeyEvent event) -> {
+               nave.getTransforms().add(new Rotate(rotacion,0,00)); 
+            
+                   switch(event.getCode()){
+                   case RIGHT:
+                       //Pulsada tecla DERECHA
+                       rotacion = -90;
+                       break;
+                   case LEFT:
+                       //Pulsada tecla IZQUIERDA
+                       rotacion = +90;
+                       break;
+                       
+                   case UP:
+                       //Pulsada tecla ARRIBA
+                       propul = +10;
+                        ;
+                       break;
+                    }
+            });
+
+        
     }
+        
+
+      
+        
+        
+        
+        
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
-    }
     
+    
+    }
 }
